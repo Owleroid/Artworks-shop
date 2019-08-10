11b51a3b2821e80757d6ddb5c66719a343e9b875
FifthWye <41546028+FifthWye@users.noreply.github.com>
2019-08-10 18:14:38 +0300

    A lot of front ( Adaptive for main page & ... )

--- public/js/main.js
+++ public/js/main.js
@@ -5,27 +5,58 @@ $(document).ready(function() {
 
   var menu = $(".nav")[0];
   menu = $(menu);
+  let mobMenubtn = $(".nav-btn");
+  let navLinks = $($(".nav-links")[0]);
+  let isMainPage = $(".slide").length;
   let showMenuPosition = 200;
 
-  let isMainPage = $(".slide").length;
+  console.log(
+    "Nav = " +
+      menu +
+      " Mob menu = " +
+      mobMenubtn.length +
+      " Links = " +
+      navLinks
+  );
+
+  for (let i = 0; i < mobMenubtn.length; i++) {
+    mobMenubtn[i].addEventListener("click", function() {
+      if (!navLinks.hasClass("show")) {
+        console.log("Menu was closed, i opening it");
+        navLinks.addClass("show");
+        if (!menu.hasClass("open")) {
+          menu.addClass("open");
+        } else {
+          if (isMainPage) {
+            menu.removeClass("open");
+          }
+        }
+      } else {
+        navLinks.removeClass("show");
+      }
+    });
+  }
 
   if (!isMainPage) {
     menu.addClass("open");
-    showMenuPosition = -10;
+  } else {
+    $(window).bind("scroll", function(e) {
+      console.log($(window).scrollTop());
+      if ($(window).scrollTop() > showMenuPosition) {
+        console.log("show menu");
+        if (!menu.hasClass("open")) {
+          console.log("well");
+          menu.addClass("open");
+        }
+      } else {
+        if (menu.hasClass("open")) {
+          if (!navLinks.hasClass("show")) {
+            menu.removeClass("open");
+          }
+        }
+      }
+    });
   }
-
-  $(window).bind("scroll", function(e) {
-    if ($(window).scrollTop() > showMenuPosition) {
-      if (!menu.hasClass("open")) {
-        console.log(menu.css("top"));
-        menu.addClass("open");
-      }
-    } else {
-      if (menu.hasClass("open")) {
-        menu.removeClass("open");
-      }
-    }
-  });
 
   //------------------------------------//
   //Scroll To//
