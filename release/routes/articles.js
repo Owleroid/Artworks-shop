173f7f5a9b02f285b19e69039670d7a158b8850c
FifthWye <41546028+FifthWye@users.noreply.github.com>
2019-09-23 20:14:09 +0300

    gitignore

--- release/routes/articles.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const articlesController = require('../controllers/articles');
-const isAuth = require('../middleware/is-auth');
-
-const router = express.Router();
-
-
-module.exports = router;
--- release/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const mainPageController = require('../controllers/main');
-
-const router = express.Router();
-
-router.get('/', mainPageController.getIndex);
-
-module.exports = router;
